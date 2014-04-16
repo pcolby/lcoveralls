5f321a573949c66a5a0e3cb440958660a513d6dc
Paul Colby <git@colby.id.au>
2014-04-16 17:23:09 +1000

    Added the --export option.

--- lib/lcoveralls/option_parser.rb
+++ lib/lcoveralls/option_parser.rb
@@ -36,15 +36,22 @@ module Lcoveralls
 
       parser = ::OptionParser.new do |o|
       o.banner = "Usage: #{o.program_name} [options] [tracefile(s)]"
-      o.summary_width = 20
+      o.summary_width = 30
       o.separator ''
 
       o.separator 'Code / coveralls.io options:'
       o.on(      '--dryrun',      'Do not submit to coveralls.io' ) { options[:dryrun] = true }
+      o.on(      '--export [PATH=stdout]', 'Export Coveralls job data') do |path|
+        options[:export] = case path
+        when 'stderr'; $stderr
+        when 'stdout'; $stdout
+        when nil ; $stdout
+        else File.new(path, 'w')
+        end
+      end
       o.on('-r', '--root PATH',   'Set the path to the repo root') { |path|  options[:root]  = File.realpath(path) }
       o.on('-s', '--service NAME','Set coveralls service name')    { |name|  options[:service] = name }
       o.on('-t', '--token TOKEN', 'Set coveralls repo token')      { |token| options[:token] = token }
-#      o.on(      '--export-json FILE'
       o.separator ''
 
       o.separator 'Stderr output options:'
@@ -55,9 +62,8 @@ module Lcoveralls
       o.on('-v', '--verbose',    'Show more output') { options[:severity] = options[:severity] - 1 }
       o.separator ''
 
-      o.on(      '--[no-]git PATH', 'Path to the git program') { |path| options[:git] = path }
-
       o.separator 'Miscellaneous options:'
+      o.on(      '--[no-]git PATH', 'Path to the git program') { |path| options[:git] = path }
       o.on('-h', '--help',    'Print usage text, then exit')     { puts o; exit }
       o.on(      '--version', 'Print version number, then exit') { puts VERSION; exit }
       o.separator ''
